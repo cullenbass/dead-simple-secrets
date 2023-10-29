bea5e7ee18eb5c749f2a749dd1066240264fa25a
Cullen Bass <cullen.bass@gmail.com>
2023-10-28 22:09:31 -0400

    Crypto scaffolding

--- crypto.go
+++ crypto.go
@@ -4,22 +4,49 @@ import (
 	"crypto/cipher"
 	"crypto/aes"
 	"crypto/rand"
-	"encoding/hex"
+	"log/slog"
+	"io"
 )
 
 type crypto struct {
-	key []byte
+	gcmCipher cipher.AEAD
 }
 
-// pass in hex string
-func NewCrypto(key string) *crypto{
-	if key == "" {
-		panic("Key is empty")
+func GenNewKey() []byte {
+	key := make([]byte, 32)
+	if _, err := io.ReadFull(rand.Reader, key); err != nil {
+		panic(err)
 	}
-	kb, _:= hex.DecodeString(key)
-	return &crypto{kb }
+	return key
 }
 
-func (c *crypto) Encrypt(s string, nonce []byte) {
-	
+func NewCrypto(key []byte) *crypto{
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		slog.Error("Cannot create cipher")
+		panic(err)
+	}
+	gcm, err := cipher.NewGCM(c) 
+	if err != nil {
+		slog.Error("Cannot create GCM")
+		panic(err)
+	}
+	return &crypto{gcm}
+}
+
+func (c *crypto) Encrypt(plaintext string) (encrypted []byte, nonce []byte){
+	nonce = make([]byte, 12)
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err)
+	}
+	encrypted = c.gcmCipher.Seal(nil, nonce, []byte(plaintext), nil)
+	return
+}
+
+func (c *crypto) Decrypt(ciphertext []byte, nonce []byte) (plaintext string) {
+	plainBytes, err := c.gcmCipher.Open(nil, nonce, ciphertext, nil);
+	if err != nil {
+		panic(err)
+	}
+	return string(plainBytes)
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
 module github.com/cullenbass/dead-simple-secrets
 
-go 1.21.3
+go 1.21.1
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/mattn/go-sqlite3 v1.14.17
+)
